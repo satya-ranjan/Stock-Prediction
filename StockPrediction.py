4f939d253562bfd898359b6d7280adcaafadd714
satya-ranjan <satyasahoocs@gmail.com>
2021-11-01 09:43:29 +0530

    first code

---
+++ StockPrediction.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+import streamlit as st
+from datetime import date
+
+
+import yfinance as yf
+
+#from fbprophet import Prophet
+#from fbprophet.plot import plot_plotly
+
+from plotly import graph_objs as go
+#import pandas_datareader as data
+
+
+START = "2010-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+
+st.title("Stock Prediction")
+
+stocks = st.text_input("Enter Stock Ticker",'AAPL')
+selected_stock =(stocks) 
+
+n_years = st.slider("Year of prediction:", 1, 4)
+period = n_years * 365
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START,TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data..")
+data = load_data(selected_stock)
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'] , name = 'stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'] , name = 'stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
